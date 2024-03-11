ec74d36ab759bc37cbcdd95844eb4d2a4071923a
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-03-11 00:01:01 +0000

    Take 2
    bench 6400564

--- src/search.cpp
+++ src/search.cpp
@@ -626,6 +626,9 @@ moves_loop:
         if (pos->checkers)
             depthReduction -= 1;
 
+        if (move == ttMove)
+            depthReduction = 0;
+
         // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
         depthReduction -= moveHistory / 16384;
 
